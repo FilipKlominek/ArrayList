2b2fc48eea8ac068475e5222952e7fe3ff8285c2
FilipKlominek <71869121+FilipKlominek@users.noreply.github.com>
2022-02-25 11:15:55 +0100

    Add files via upload

--- Kotlin_ArrayList/src/main/kotlin/ListArray.kt
+++ Kotlin_ArrayList/src/main/kotlin/ListArray.kt
@@ -1,5 +1,5 @@
 class ListArray<T>(
-    var content: Array<T>
+    var content: Array<Any>
 ) {
     fun size(): Int {
         return content.size
@@ -10,36 +10,34 @@ class ListArray<T>(
     }
 
     fun indexOf(item: T): Int {
-        return if (!content.contains(item)) -1
-        else content.indexOf(item)
+        return if (!content.contains(item as Any)) -1
+        else content.indexOf(item as Any)
     }
 
-    fun toArray(): Array<T> {
+    fun toArray(): Array<Any> {
         return content
     }
 
-    fun get(index: Int): T {
+    fun get(index: Int): Any {
         return content[index]
     }
 
-
     fun add(item: T) {
         val list = content.toMutableList()
-        list.add(item)
-
+        list.add(item as Any)
         content = list.toTypedArray()
     }
 
     fun set(index: Int, item: T) {
-        content[index] = item
+        content[index] = item as Any
     }
 
     fun contains(item: T): Boolean {
-        return content.contains(item)
+        return content.contains(item as Any)
     }
 
     fun lastIndexOf(item: T): Int {
-        return if (!content.contains(item)) -1
-        else content.lastIndexOf(item)
+        return if (!content.contains(item as Any)) -1
+        else content.lastIndexOf(item as Any)
     }
 }
